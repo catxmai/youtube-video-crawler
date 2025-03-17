a129fd357ff7b6ff8b001a3f9f736b8e21f33b9d
Cat Mai <catmaimx@gmail.com>
2025-03-17 18:55:52 -0400

    add options for linux

--- utils.py
+++ utils.py
@@ -64,11 +64,14 @@ def create_driver(headless:bool, user_data_dir="") -> webdriver.Chrome:
     options.add_argument("--window-size=1540,1080")
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
 
     if headless:
         options.add_argument("--headless=new")
 
-    if user_data_dir:
+    if len(user_data_dir) > 0:
         options.add_argument("user-data-dir=" + user_data_dir)
 
     try:
